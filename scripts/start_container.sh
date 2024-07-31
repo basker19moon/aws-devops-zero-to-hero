e872619e02929c041719c7ed6e543c7f88286691
Bhaskar Darla <52462387+basker19moon@users.noreply.github.com>
2024-07-31 23:22:52 +0530

    add the files for codedeploy

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull basker19moon/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 basker19moon/simple-python-flask-app
